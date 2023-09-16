b7835ce1d10dcdfe1d48d0d8710dcf2cba90ce45
Alexandr <sasha_aleshin@mail.ru>
2023-09-17 00:50:19 +0300

    hw-4 js

---
+++ hw-4 js/script.js
@@ -0,0 +1,33 @@
+for (let i = 1; i <= 2; i++) {
+    console.log('Привет');
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+};
+
+for (let name in obj) {
+    console.log(`${name} — зарплата ${obj[name]}`);
+}
+
+let n = 1000;
+let num = 5;
+
+while (n > 50) {
+    n = n / 2;
+}
+console.log(`Получилось число ${n}. Число итераций равно ${num}`);
+
+for (let dayMonth = 1; dayMonth <= 31; dayMonth += 7) {
+    console.log(`Сегодня пятница, ${dayMonth}-е число. Необходимо подготовить отчет.`);
+}
