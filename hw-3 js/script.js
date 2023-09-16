007c6f17631af5d4132353da645fd233f7e39d19
Alexandr <sasha_aleshin@mail.ru>
2023-09-16 16:02:12 +0300

    исправлено case

--- hw-3 js/script.js
+++ hw-3 js/script.js
@@ -27,17 +27,43 @@ let b = '3';
 alert(Number(a) + Number(b));
 
 let varMonth = Number(prompt("Введите номер месяца от 1 до 12"));
-switch (varMonth) { 
-case 1: alert( 'Зима' ); break;
-case 2: alert( 'Зима' ); break;
-case 3: alert( 'весна' ); break;
-case 4: alert( 'весна' ); break;
-case 5: alert( 'весна' ); break;
-case 6: alert( 'лето' ); break;
-case 7: alert( 'лето' ); break;
-case 8: alert( 'лето' ); break;
-case 9: alert( 'осень' ); break;
-case 10: alert( 'осень' ); break;
-case 11: alert( 'осень' ); break;
-case 12: alert( 'Зима' ); break;
-default: alert( 'Неизвестное значение' );}
+switch (varMonth) {
+    case 1:
+        alert('Зима');
+        break;
+    case 2:
+        alert('Зима');
+        break;
+    case 3:
+        alert('весна');
+        break;
+    case 4:
+        alert('весна');
+        break;
+    case 5:
+        alert('весна');
+        break;
+    case 6:
+        alert('лето');
+        break;
+    case 7:
+        alert('лето');
+        break;
+    case 8:
+        alert('лето');
+        break;
+    case 9:
+        alert('осень');
+        break;
+    case 10:
+        alert('осень');
+        break;
+    case 11:
+        alert('осень');
+        break;
+    case 12:
+        alert('Зима');
+        break;
+    default:
+        alert('Неизвестное значение');
+}
