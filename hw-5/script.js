7f968890f557904b372ec9e2f3c8519bad8b6d5a
Alexandr <sasha_aleshin@mail.ru>
2023-09-22 19:58:49 +0300

    добавил игру

---
+++ hw-5/script.js
@@ -0,0 +1,122 @@
+const smallNumber = (a,b) => {
+	if (a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+smallNumber(2,4);
+
+
+const checkEvenNumb = (a) => {
+	if (a % 2 === 0) {
+		return 'Число четное';
+	} else {
+		return 'Число НЕчетное';
+	}
+}
+
+checkEvenNumb(5);
+
+
+const logToConsole = (a) => {
+	console.log(a * a);
+}
+
+logToConsole(4);
+
+
+const retunToConsole = (a) => {
+	return a * a;
+}
+
+retunToConsole(4);
+
+
+const checkAge = (a) => {
+	if (a < 0) {
+		alert('Вы ввели неправильное значение');
+	}
+
+	if (a >= 0 && a <= 12) {
+		alert('привет, друг!');
+	}
+
+	if (a >= 13) {
+		alert('Добро пожаловать!');
+	}
+}
+
+checkAge(prompt('Введите свой возраст'));
+
+
+const checkNumber = (a,b) => {
+	if (isNaN(a) || isNaN(b)) {
+		return 'Одно или оба значения не являются числом';
+	} else {
+		return a * b;
+	}
+}
+
+checkNumber(4,4);
+
+
+const exponentiation = (a) => {
+	if (isNaN(a)) {
+		alert('Переданный параметр не является числом');
+	} else {
+		alert(`${a} в кубе равняется ${a ** 3}`);
+	}
+}
+
+exponentiation(prompt('Введите число'));
+
+
+function getCircleArea() {
+	return this.radius ** 2 * Math.PI;
+}
+
+function getCirclePerimiter() {
+ return this.radius * 2 * Math.PI; 
+}
+
+const circle1 = {
+	radius:4,
+	getArea: getCircleArea,
+	getPerimiter: getCirclePerimiter,
+}
+
+const circle2 = { 
+	radius:8,
+	getArea: getCircleArea,
+	getPerimiter: getCirclePerimiter,
+}
+
+	console.log(`Площадь круга 1 равна ${circle1.getArea()}`);
+	console.log(`Периметр круга 1 равен ${circle1.getPerimiter()}`);
+	console.log(`Площадь круга 2 равна ${circle2.getArea()}`);
+	console.log(`Периметр круга 2 равен ${circle2.getPerimiter()}`);
+
+
+const showSeason = (a) => {
+	if (a === 1 || a === 2 || a === 12) {
+		return 'Зима';
+
+	} else if (a === 3 || a === 4 || a === 5) {
+		return 'Весна';
+	}
+
+	else if (a === 6 || a === 7 || a === 8) {
+		return 'Лето';
+	}
+
+	else if (a === 9 || a === 10 || a === 11) {
+		return 'Осень';
+
+	} else {
+		return 'Вы ввели что-то не то';
+	}
+}
+
+showSeason(Number(prompt('Введите значение от 1 до 12')));
